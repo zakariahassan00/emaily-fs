9b6c0126865566340b4f220d10d17114d15a10da
Zakaria Hassan <zakariahassan00@hotmail.com>
2019-09-25 18:35:32 +0200

    Fixed typo in index.js #2

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
 // deployment with react app settings
-if (process.env.NODDE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   // express will serve up production assets like our main.js file, or main.css file!.
   app.use(express.static("client/build"));
 
